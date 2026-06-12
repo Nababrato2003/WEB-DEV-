b9cc5cb08d431a5b516eea2192c2eef979d35a9a
Nababrato2003 <nababratobiswas.11c@gmail.com>
2026-06-12 22:29:14 +0530

    12_06_2026

---
+++ Frontend/JS/1_Variables and Declarations/script.js
@@ -0,0 +1,10 @@
+var a;
+var a = 12;
+let a;
+let a = 12;
+const a= 12;
+const a; //throws error 
+
+//declarations and initializations
+var a; //declaration
+var a = 12; //declaration and initialization
