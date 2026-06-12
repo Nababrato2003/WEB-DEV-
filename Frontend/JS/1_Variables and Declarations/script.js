a43f7d4cf79ca0a200b247c3d33d7ebb02699d77
Nababrato2003 <nababratobiswas.11c@gmail.com>
2026-06-12 22:40:51 +0530

    hola

--- Frontend/JS/1_Variables and Declarations/script.js
+++ Frontend/JS/1_Variables and Declarations/script.js
@@ -8,3 +8,8 @@ const a; //throws error
 //declarations and initializations
 var a; //declaration
 var a = 12; //declaration and initialization
+
+let a = 12//always use for variable declaration , jodi dubar declare kora hoy tahole error dibe
+
+const a = 12; //always set kora hoya galo , it cannot be changed later 
+//Let hole value or decalation value can be changed but const hole value change kora jabe nah . 
