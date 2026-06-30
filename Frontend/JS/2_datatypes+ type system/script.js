00f6df829b3ec419390025cdbbaf978b4c780d8e
Nababrato2003 <nababratobiswas.11c@gmail.com>
2026-06-30 23:59:17 +0530

    doing

---
+++ Frontend/JS/2_datatypes+ type system/script.js
@@ -0,0 +1,5 @@
+//Data types in JS
+//primitives --> number, string, boolean, null, undefined, symbol
+//primitive -->  the values which when we copy , we can get the real copy of the value
+//Reference --> after copying yhose value , we will not get real copy but we will get the reference of the parent value
+// reference --> object, array, function
