009a0c5a55e0e3f52323c2632ae4099bb366f20e
Nababrato2003 <nababratobiswas.11c@gmail.com>
2026-07-12 20:15:19 +0530

    3hours JS done

---
+++ Frontend/JS/4_Control_flow/topic1.js
@@ -0,0 +1,31 @@
+//if else, else if 
+
+if (65) {
+    console.log("passed");
+}
+else {
+    console.log("not passed")
+}
+//here jokhon condition dawa nei then the value which is given in the condition sector (65) , otar truthy or false nature determine kore if else condition ar moddha dhukbe nahole dhukbe nah
+//this in case of absence of any condition , JS checks the truthy or false nature of the value which is given in the condition sector 
+
+if (12 > 17) {
+    console.log("This block will be executed");
+}
+else {
+    console.log("else block will be executed");
+}
+
+
+
+
+if (loggedin && admin) {
+
+}
+else if (loggedin) {
+
+}
+else {
+
+}
+//as much as the user likes else blocks can be made 
