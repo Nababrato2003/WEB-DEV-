efd3524231d23fcd4e3699a57d3ce8720ca3e5f0
Nababrato2003 <nababratobiswas.11c@gmail.com>
2026-07-30 22:47:35 +0530

    backend resources added for later purpose

---
+++ Frontend/JS/5_loops/topic1.js
@@ -0,0 +1,15 @@
+//repeat --> loops useage 
+// kotha theke jeta hobe --> koto ta opdi jeta hobe --> ki bhabe jabo---> use for loop
+
+//kotha theke jeta hobe -> kokhon stop krte hobe -> ki bhabe jeta hobe ---> use while loop 
+//while loop tokhon use krbe jokhon , jokhon kono particular iterations jana thakbe na tokhon while loop use kora hobe , LIKE there is no certainnity kokhon destination ta pabo 
+
+
+//for loop
+for(start;end;change){
+    
+}
+
+for((let i = 0; i<10; i++)){
+    console.log("hello world")
+}
